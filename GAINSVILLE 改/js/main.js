7339a70a1e96c5a3e5c86777cec1beb161af2f92
Tianshu439 <85494720+Tianshu439@users.noreply.github.com>
2021-06-09 17:51:45 +0800

    Delete GAINSVILLE 改 directory

--- GAINSVILLE 改/js/main.js
+++
@@ -1,4 +0,0 @@
-$(".menu ul li").click(function(){
-    $(this).find("dl").slideToggle(200).parent().siblings().find("dl").slideUp(200);
-    $(this).find("span i").toggleClass('action').parents().siblings().find("span i").removeClass('action');
-})
